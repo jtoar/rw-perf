5dabc22547591176025407a0f5608db18639c60f
Dominic Saadi <dominiceliassaadi@gmail.com>
2023-11-30 13:44:58 -0800

    tweak buildHandler for clinic

--- wip.sh
+++ wip.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 
+set -x
+
+cd test-project-6.4.1 || exit 1
+# npx clinic heapprofiler -- node ./node_modules/.bin/rw build --no-prerender
+npx clinic doctor -- node ./node_modules/.bin/rw build --no-prerender
