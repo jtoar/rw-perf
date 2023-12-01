b626851ae488408517d2da3ade1692871c837f4e
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2023-12-01 23:06:49 +0000

    wip

--- scripts/visualise/visualise.js
+++ scripts/visualise/visualise.js
@@ -1,166 +1,264 @@
 
-async function main() {
+async function main(divisionCount = 16) {
   const rawData = JSON.parse(rawDataJSON)
+  const rawArgs = JSON.parse(rawArgsJSON)
 
   // Put raw data into the pre tag
   const pre = document.getElementById("raw-data")
   pre.innerHTML = JSON.stringify(rawData, undefined, 2)
 
-  // Timestamps
-  const times = []
-  const totalMemory = []
-  for (let i = 0; i < rawData.length; i++) {
-    const anyPid = Object.keys(rawData[i]).filter(pid => pid !== 'total' && pid !== 'totalMB')[0]
-    times.push(rawData[i][anyPid].timestamp)
-    totalMemory.push(rawData[i].totalMB)
+  // PID details
+  const pidDetails = document.getElementById("pid-details")
+  for (const [pid, rawArg] of Object.entries(rawArgs)) {
+    const arg = atob(rawArg)
+
+    const row = document.createElement("div")
+    row.className = "flex flex-col gap-2"
+
+    const pidEl = document.createElement("div")
+    pidEl.className = "font-bold"
+    pidEl.innerText = pid
+    row.appendChild(pidEl)
+
+    const argsEl = document.createElement("div")
+    argsEl.innerText = ` - Args: ${arg}`
+    argsEl.className = "whitespace-nowrap"
+    row.appendChild(argsEl)
+
+    const dataPoints = rawData[pid].length
+    const dataPointsEl = document.createElement("div")
+    dataPointsEl.innerText = ` - Data points: ${dataPoints}`
+    dataPointsEl.className = "whitespace-nowrap"
+    row.appendChild(dataPointsEl)
+
+    const max = (Math.max(...rawData[pid].map(p => p.memory)) / 1024 / 1024).toFixed(3)
+    const maxEl = document.createElement("div")
+    maxEl.innerText = ` - Max: ${max} MB`
+    maxEl.className = "whitespace-nowrap"
+    row.appendChild(maxEl)
+
+    const mean = (rawData[pid].reduce((acc, p) => acc + p.memory, 0) / rawData[pid].length / 1024 / 1024).toFixed(3)
+    const meanEl = document.createElement("div")
+    meanEl.innerText = ` - Mean: ${mean} MB`
+    meanEl.className = "whitespace-nowrap"
+    row.appendChild(meanEl)
+
+    const sorted = rawData[pid].map(p => p.memory).sort()
+    const median = (sorted[Math.floor(sorted.length / 2)] / 1024 / 1024).toFixed(3)
+    const medianEl = document.createElement("div")
+    medianEl.innerText = ` - Median: ${median} MB`
+    medianEl.className = "whitespace-nowrap"
+    row.appendChild(medianEl)
+
+    const lifetime = (rawData[pid][rawData[pid].length - 1].timestamp - rawData[pid][0].timestamp) / 1000
+    const lifetimeEl = document.createElement("div")
+    lifetimeEl.innerText = ` - Lifetime: ${lifetime} seconds`
+    lifetimeEl.className = "whitespace-nowrap"
+    row.appendChild(lifetimeEl)
+
+    pidDetails.appendChild(row)
   }
 
-  const zeroAdjustedTimes = times.map(t => t - times[0])
+  // Stats
+  const stats = document.getElementById("stats")
 
-  // Totals chart
-  datasetTotal = {
-    label: "Total",
-    data: totalMemory,
-    borderWidth: 1,
-    fill: 'origin'
+  const pidCount = Object.keys(rawData).length
+  const pidCountEl = document.createElement("div")
+  pidCountEl.innerText = `PID count: ${pidCount}`
+  pidCountEl.className = "whitespace-nowrap"
+  stats.appendChild(pidCountEl)
+
+  // Individual chart
+
+  const allTimestamps = new Set()
+  for (const pid of Object.keys(rawData)) {
+    for (const dataPoint of rawData[pid]) {
+      allTimestamps.add(dataPoint.timestamp)
+    }
+  }
+  const sortedTimestamps = Array.from(allTimestamps).sort()
+  const zeroAdjustedTimes = sortedTimestamps.map(t => t - sortedTimestamps[0])
+
+  const datasets = []
+  for (const pid of Object.keys(rawData)) {
+    const dataset = {
+      label: pid,
+      data: [],
+      fill: true,
+      tension: 0.4
+    }
+    for (const timestamp of sortedTimestamps) {
+      const dataPoint = rawData[pid].find(p => p.timestamp === timestamp)
+      if (dataPoint === undefined) {
+        dataset.data.push(null)
+      } else {
+        dataset.data.push(dataPoint.memory / 1024 / 1024)
+      }
+    }
+    datasets.push(dataset)
+  }
+
+  const ctxIndi = document.getElementById('chart-indi');
+  new Chart(ctxIndi, {
+    type: 'line',
+    data: {
+      labels: zeroAdjustedTimes,
+      datasets: datasets,
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          display: true,
+          position: 'right',
+        },
+      },
+      spanGaps: true,
+      scales: {
+        y: {
+          title: {
+            display: true,
+            text: 'RSS (megabytes)'
+          },
+          suggestedMin: 0,
+        },
+        x: {
+          title: {
+            display: true,
+            text: 'Time (milliseconds)'
+          },
+        }
+      }
+    },
+  });
+
+  // Total chart
+  const divisions = Math.floor((zeroAdjustedTimes[zeroAdjustedTimes.length - 1] / 1000) * divisionCount)
+
+  const totalTimestamps = [0]
+  const deltaTime = zeroAdjustedTimes[zeroAdjustedTimes.length - 1] / divisions
+  for (let i = 1; i <= divisions; i++) {
+    totalTimestamps.push(Math.round((totalTimestamps[i - 1] + deltaTime)))
+  }
+  const totalData = []
+  for (let i = 1; i < totalTimestamps.length; i++) {
+    const minT = totalTimestamps[i - 1] + sortedTimestamps[0]
+    const maxT = totalTimestamps[i] + sortedTimestamps[0]
+    let total = 0
+    for (const pid of Object.keys(rawData)) {
+      let max = 0
+      for (const dataPoint of rawData[pid]) {
+        if (dataPoint.timestamp >= minT && dataPoint.timestamp <= maxT) {
+          max = Math.max(max, dataPoint.memory)
+        }
+      }
+      total += max
+    }
+    totalData.push(total === 0 ? null : total / 1024 / 1024)
   }
 
   const ctxTotal = document.getElementById('chart-total');
   new Chart(ctxTotal, {
     type: 'line',
     data: {
-      labels: zeroAdjustedTimes,
-      datasets: [datasetTotal]
+      labels: totalTimestamps,
+      datasets: [{
+        label: 'Total',
+        data: totalData,
+        fill: true,
+        tension: 0.4
+      }],
     },
     options: {
       responsive: true,
       plugins: {
         legend: {
-          display: false,
+          display: true,
+          position: 'right',
         },
       },
+      spanGaps: true,
       scales: {
         y: {
           title: {
             display: true,
             text: 'RSS (megabytes)'
           },
+          suggestedMin: 0,
         },
         x: {
           title: {
             display: true,
             text: 'Time (milliseconds)'
-          }
+          },
         }
       }
     },
   });
 
-  // Totals sidebar
-  document.getElementById('total-count').innerHTML = rawData.length
-  document.getElementById('total-max').innerHTML = Math.max(...totalMemory).toFixed(3)
-  document.getElementById('total-mean').innerHTML = (totalMemory.reduce((a, b) => a + b) / totalMemory.length).toFixed(3)
-  document.getElementById('total-median').innerHTML = (totalMemory.sort((a, b) => a - b)[Math.floor(totalMemory.length / 2)]).toFixed(3)
-
-  // Chart of individual processes
-  const datasets = []
-  const pids = new Set()
-  for (let i = 0; i < rawData.length; i++) {
-    const keys = Object.keys(rawData[i])
-    for (let j = 0; j < keys.length; j++) {
-      if (keys[j] !== 'total' && keys[j] !== 'totalMB') {
-        pids.add(keys[j])
+  // Indi binned chart
+  const indiBinnedData = new Map()
+  for (const pid of Object.keys(rawData)) {
+    indiBinnedData.set(pid, [])
+  }
+  for (let i = 1; i < totalTimestamps.length; i++) {
+    const minT = totalTimestamps[i - 1] + sortedTimestamps[0]
+    const maxT = totalTimestamps[i] + sortedTimestamps[0]
+    for (const pid of Object.keys(rawData)) {
+      let max = 0
+      for (const dataPoint of rawData[pid]) { 
+        if (dataPoint.timestamp >= minT && dataPoint.timestamp <= maxT) {
+          max = Math.max(max, dataPoint.memory)
+        }
       }
+      indiBinnedData.get(pid).push(max === 0 ? null : max / 1024 / 1024)
     }
   }
-  for (const pid of pids) {
-    const data = []
-    for (let i = 0; i < rawData.length; i++) {
-      data.push((rawData[i][pid]?.memory ?? 0) / 1024 / 1024)
-    }
-    datasets.push({
+
+  const indiBinnedDatasets = []
+  for (const [pid, data] of indiBinnedData) {
+    indiBinnedDatasets.push({
       label: pid,
       data: data,
-      borderWidth: 1,
-      fill: 'origin'
+      fill: true,
+      tension: 0.4
     })
   }
 
-  const ctx = document.getElementById('chart-indi');
-  new Chart(ctx, {
+  const ctxIndiBinned = document.getElementById('chart-indi-binned');
+  new Chart(ctxIndiBinned, {
     type: 'line',
     data: {
-      labels: zeroAdjustedTimes,
-      datasets: datasets
+      labels: totalTimestamps,
+      datasets: indiBinnedDatasets,
     },
     options: {
       responsive: true,
       plugins: {
         legend: {
-          position: 'top',
+          display: true,
+          position: 'right',
         },
       },
+      spanGaps: true,
       scales: {
         y: {
           title: {
             display: true,
             text: 'RSS (megabytes)'
           },
-          stacked: true
+          suggestedMin: 0,
+          stacked: true,
         },
         x: {
           title: {
             display: true,
             text: 'Time (milliseconds)'
-          }
+          },
         }
       }
     },
   });
-
-  // Add pid details 
-  const container = document.getElementById('pid-details')
-
-  const pidArgs = new Map(JSON.parse(pidArgsJSON))
-  for (let [pid, rawArg] of pidArgs) {
-    const data = rawData.map(d => d[pid]?.memory ?? -1).filter(d => d !== -1).map(d => d / 1024 / 1024)
-    const dataPoints = data.length
-    const max = Math.max(...data).toFixed(3)
-    const mean = (data.reduce((a, b) => a + b) / data.length).toFixed(3)
-    const median = data.sort((a, b) => a - b)[Math.floor(data.length / 2)].toFixed(3)
-    const arg = atob(rawArg).trim() || '?'
-
-    const row = document.createElement("div")
-    row.className = 'flex flex-col'
-
-    const pidDiv = document.createElement("div")
-    pidDiv.appendChild(document.createTextNode(pid))
-    pidDiv.className = 'font-bold'
-    row.appendChild(pidDiv)
-
-    const argDiv = document.createElement("div")
-    argDiv.appendChild(document.createTextNode(`- Args: ${arg}`))
-    argDiv.className = 'whitespace-nowrap'
-    row.appendChild(argDiv)
-
-    const dataPointsDiv = document.createElement("div")
-    dataPointsDiv.appendChild(document.createTextNode(`- Data points: ${dataPoints}`))
-    row.appendChild(dataPointsDiv)
-
-    const maxDiv = document.createElement("div")
-    maxDiv.appendChild(document.createTextNode(`- Max: ${max}`))
-    row.appendChild(maxDiv)
-
-    const meanDiv = document.createElement("div")
-    meanDiv.appendChild(document.createTextNode(`- Mean: ${mean}`))
-    row.appendChild(meanDiv)
-
-    const medianDiv = document.createElement("div")
-    medianDiv.appendChild(document.createTextNode(`- Median: ${median}`))
-    row.appendChild(medianDiv)
-
-    container.appendChild(row)
-  }
 
 }
 
